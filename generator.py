f068531f9ac9254eb6223515f06506e7e46a7cc3
McgahGuhmer <80374341+McgahGuhmer@users.noreply.github.com>
2021-05-27 14:24:40 -0700

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,20 @@
+import random
+bigNum = randint(0,100000)
+smallNum = randint(0, 100)
+probType = randint(0, 3)
+if probType == 0
+  print(bigNum)
+  print("+")
+  print(smallNum)
+if probType == 1
+  print(bigNum)
+  print("-")
+  print(smallNum)
+if probType == 2
+  print(bigNum)
+  print("*")
+  print(smallNum)
+if probType == 3
+  print(bigNum)
+  print("/")
+  print(smallNum)
